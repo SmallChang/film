7e4633147417841746a6178b8c89907293b8bb1e
畅丽瑶 <1651108642@qq.com>
2019-11-05 19:49:52 +0800

    添加readme

---
+++ src/test/java/org/cly/FilmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
